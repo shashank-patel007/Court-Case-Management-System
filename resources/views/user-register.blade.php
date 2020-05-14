8b338175a62bb2ef251a1d0eaeb3b6decfcdd629
shashank-patel007 <shashankcr777@gmail.com>
2020-05-14 19:51:22 +0530

    Super final

--- resources/views/user-register.blade.php
+++ resources/views/user-register.blade.php
@@ -42,6 +42,33 @@
                 </div>
 
                 <div class="form-group row">
+                  <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Number') }}</label>
+
+                  <div class="col-md-6">
+                    <input id="name" type="tel" class="form-control @error('name') is-invalid @enderror" name="number" value="{{ old('number') }}" required autocomplete="number" autofocus>
+
+                    @error('number')
+                    <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                  </div>
+                </div>
+
+                <div class="form-group row">
+                  <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Address') }}</label>
+
+                  <div class="col-md-6">
+                    <input id="name" type="text" class="form-control @error('address') is-invalid @enderror" name="address" value="{{ old('address') }}" required autocomplete="address" autofocus>
+
+                    @error('address')
+                    <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                  </div>
+                </div>
+                <div class="form-group row">
                   <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                   <div class="col-md-6">
